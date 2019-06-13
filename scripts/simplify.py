cd45755ebbdde325acb06dce98381412affa47c0
rogerpt32 <rogerpt32@gmail.com>
2019-06-13 15:54:10 +0200

    Add simplifier script

---
+++ scripts/simplify.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv)!=2:
+    exit(-1)
+
+file = sys.argv[1]
+with open(file) as f:
+    for line in f.readlines():
+        if '>' not in line:
+            print(line.replace('\n',''),end='')
