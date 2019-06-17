c12ba9c0c17cb4fc17326b7c7e60d24ce33d5e62
rogerpt32 <rogerpt32@gmail.com>
2019-06-17 23:56:24 +0200

    Add plots

--- scripts/get_results.py
+++ scripts/get_results.py
@@ -1,5 +1,15 @@
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+rcParams['font.family'] = 'serif'
+rcParams['font.size'] = 10
+rcParams['font.sans-serif'] = ['Console Modern']
+rcParams['savefig.format'] = ['pdf']
+rcParams['savefig.bbox'] = 'tight'
+rcParams['savefig.pad_inches'] = 0
+
 
 if len(sys.argv)!=2:
     exit(-1)
@@ -33,4 +43,37 @@ with open(file) as f:
                 data.append(row)
 
 df=pd.DataFrame(data,columns=['size','gzip_size','gzip_time','paper_size','paper_time'])
+
 df.to_csv('../results/results.csv',index=False)
+
+df['gzip_percent'] = 100*df['gzip_size']/df['size']
+df['paper_percent'] = 100*df['paper_size']/df['size']
+
+fig, ax = plt.subplots()
+ax.set_xscale('log', basex=2)
+plt.ylim(-5,125)
+plt.xlabel('Size (B)')
+plt.ylabel('Compression level (%)')
+ax.plot(df['size'],df['gzip_percent'],label='gzip')
+ax.plot(df['size'],df['paper_percent'],label='paper')
+plt.legend()
+plt.tight_layout()
+fig.savefig("../plots/compression.pdf")
+#plt.show()
+plt.cla()
+plt.clf()
+plt.close()
+
+fig, ax = plt.subplots()
+ax.set_xscale('log', basex=2)
+plt.xlabel('Size (B)')
+plt.ylabel('Time (s)')
+ax.plot(df['size'],df['gzip_time'],label='gzip')
+ax.plot(df['size'],df['paper_time'],label='paper')
+plt.legend()
+plt.tight_layout()
+fig.savefig("../plots/time.pdf")
+#plt.show()
+plt.cla()
+plt.clf()
+plt.close()
