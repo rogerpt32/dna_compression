1dfae25c9cea2fbdbcf3ca36565afb4f7e2caa79
rogerpt32 <rogerpt32@gmail.com>
2019-06-18 16:45:25 +0200

    Add cyfar

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -11,21 +11,29 @@ for i in `seq 1 30`;
 do
     for j in `seq 1 30`;
     do
-        rm -f ../data/test.gz ../data/test_compressed.dna
+        rm -f ../data/test.gz ../data/test_compressed.dna ../data/test.cryfa
         pow=$( echo "2^$i" | bc )
         c=$(( 30*(i-1) + j ))
         echo -ne "$(printf %03d $c)/900 \t size $pow"\\r
         echo -e "\n\nSize $pow\n" >> ../results/results.txt
-        ./generator $pow ../data/test.txt
+        ./generator $pow ../data/test.txt ../data/test.fa
         echo -e "gzip:" >> ../results/results.txt
-        { time tar -czvf ../data/test.gz ../data/test.txt >/dev/null; } 2>&1 | grep real >> ../results/results.txt
+        { time tar -czvf ../data/test.gz ../data/test.fa >/dev/null; } 2>&1 | grep real >> ../results/results.txt
         echo -ne "compr " >> ../results/results.txt
         stat --printf="%s" ../data/test.gz >> ../results/results.txt
         echo -e "\nour:" >> ../results/results.txt
         { time ./compress ../data/test.txt >/dev/null; } 2>&1 | grep real >> ../results/results.txt
         echo -ne "compr " >> ../results/results.txt
         stat --printf="%s" ../data/test_compressed.dna >> ../results/results.txt
-        
+
+        # if [ 7 -le $i ]
+        # then
+        #     echo -e "\ncryfa:" >> ../results/results.txt
+        #     ../../cryfa/cryfa -k ../scripts/pass.txt ../data/test.fa > ../data/test.cryfa 2>/dev/null
+        #     { time ../../cryfa/cryfa -k ../scripts/pass.txt ../data/test.txt >/dev/null; } 2>&1 | grep real >> ../results/results.txt
+        #     echo -ne "compr " >> ../results/results.txt
+        #     stat --printf="%s" ../data/test.cryfa >> ../results/results.txt
+        # fi
     done
 done
 
--- src/generator.c
+++ src/generator.c
@@ -4,14 +4,16 @@
 #include <time.h>
 
 int main (int argc, char *argv[]) {
-    if(argc!=3){
+    if(argc!=3 && argc!=4){
         fprintf(stderr,"Error: Wrong number of arguments\n");
         return -1;
     }
     int n = atoi(argv[1]);
     char* fname = argv[2];
+    char* fname2;
     char labels[4]={'A','C','G','T'};
     time_t t;
+    int counter=0;
 
     /* Intializes random number generator */
     srand((unsigned) time(&t));
@@ -21,11 +23,29 @@ int main (int argc, char *argv[]) {
         fprintf(stderr,"Error: Opening file to write %s\n",fname);
         return -1;
     }
+    FILE *file2;
+    if(argc==4){
+        fname2=argv[3];
+        if((file2 = fopen(fname2, "w")) == NULL){
+            fprintf(stderr,"Error: Opening file to write %s\n",fname2);
+            return -1;
+        }
+        fprintf(file2, "> random_file  %d nb\n",n);
+    }
 
     for(int i = 0 ; i < n ; i++ ) {
-        putc((int)labels[rand()%4],file);
+        int aux=rand()%4;
+        putc((int)labels[aux],file);
+        if(argc==4){
+            putc((int)labels[aux],file2);
+            ++counter;
+            if(counter>=79){
+                fprintf(file2, "\n");
+                counter=0;
+            }
+        }
     }
     fclose(file);
-
+    if(argc==4)fclose(file2);
     return 0;
 }
